299d18b316a39ded244b56c68afcf8ff4053eb50
devcybexoft <dev@cybexoft.com.ua>
2023-09-10 22:43:32 +0300

    homework after debug with RxJava

--- app/src/main/java/com/example/weatherproject/ApiClient.kt
+++ app/src/main/java/com/example/weatherproject/ApiClient.kt
@@ -2,13 +2,22 @@ package com.example.weatherproject
 
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    val retrofit = Retrofit.Builder()
+    /*val retrofit:Retrofit = Retrofit.Builder()
         .baseUrl("https://goweather.herokuapp.com/weather/")
         .client(OkHttpClient.Builder().build())
         .addConverterFactory(GsonConverterFactory.create())
+        .build()*/
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://goweather.herokuapp.com/weather/")
+        .client(OkHttpClient.Builder().build())
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .client(OkHttpClient())
         .build()
 }
--- app/src/main/java/com/example/weatherproject/ApiInterface.kt
+++ app/src/main/java/com/example/weatherproject/ApiInterface.kt
@@ -1,8 +1,12 @@
 package com.example.weatherproject
+import io.reactivex.Single
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiInterface {
     @GET("/Kyiv")
     fun getWeather():Call<WeatherResponse>
+
+    @GET("Kyiv")
+    fun getWeatherRx(): Single<WeatherResponse>
 }
